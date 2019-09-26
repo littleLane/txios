e4784b5aacdd856db228020f0cfaf972e118e0c8
littleLane <lzz857183384@163.com>
2019-09-26 23:36:44 +0800

    feat: xhr 改造

--- src/core/xhr.ts
+++ src/core/xhr.ts
@@ -24,97 +24,126 @@ export default function xhr(config: TxiosRequestConfig): TxiosPromise {
 
     const xhrequest = new XMLHttpRequest()
 
-    if (withCredentials) {
-      xhrequest.withCredentials = true
-    }
-
-    if ((withCredentials || isURLSameOrigin(url!)) && xsrfCookieName) {
-      const xsrfValue = cookie.read(xsrfCookieName)
-
-      if (xsrfValue) {
-        headers[xsrfHeaderName!] = xsrfValue
-      }
-    }
-
-    if (typeof onDownloadProgress === 'function') {
-      xhrequest.onprogress = onDownloadProgress
-    }
-
-    if (typeof onUploadProgress === 'function') {
-      xhrequest.upload.onprogress = onUploadProgress
-    }
-
-    if (responseType) {
-      xhrequest.responseType = responseType
-    }
-
     // 开启一个链接
     xhrequest.open(method.toUpperCase(), url!, true)
 
-    // 监听状态变化
-    xhrequest.onreadystatechange = function handleLoad() {
-      if (xhrequest.readyState !== 4) {
-        return
-      }
+    configureRequest()
 
-      // 网络出错或者超时
-      if (xhrequest.status === 0) {
-        return
-      }
+    addEvents()
 
-      const responseHeaders = parseHeaders(xhrequest.getAllResponseHeaders())
-      const responseData = responseType && responseType !== 'text' ? xhrequest.response : xhrequest.responseText
-      const response: TxiosResponse = {
-        data: responseData,
-        status: xhrequest.status,
-        statusText: xhrequest.statusText,
-        headers: responseHeaders,
-        config,
-        request: xhrequest
-      }
+    processHeaders()
 
-      handleResponse(response)
-    }
-
-    // 网络错误处理
-    xhrequest.onerror = function() {
-      reject(createError(
-        'Network Error',
-        config,
-        null,
-        xhrequest
-      ))
-    }
-
-    // 请求超时处理
-    if (timeout) {
-      xhrequest.timeout = timeout;
-    }
-
-    xhrequest.ontimeout = function() {
-      reject(createError(
-        `Timeout of ${timeout} ms exceeded`,
-        config,
-        'ECONNABORTED',
-        xhrequest
-      ))
-    }
-
-    // 设置请求头
-    Object.keys(headers).forEach((key: string) => {
-      if (data === null && key.toLowerCase() === 'content-type') {
-        delete headers[key]
-      } else {
-        xhrequest.setRequestHeader(key, headers[key])
-      }
-    })
-
-    if (isFormData(data)) {
-      delete headers['Content-Type']
-    }
+    processCancel()
 
     // 发送请求
     xhrequest.send(data)
+
+    // 配置参数
+    function configureRequest(): void {
+      // 响应数据类型
+      if (responseType) {
+        xhrequest.responseType = responseType
+      }
+
+      // 请求超时处理
+      if (timeout) {
+        xhrequest.timeout = timeout;
+      }
+
+      // cookie 凭证
+      if (withCredentials) {
+        xhrequest.withCredentials = true
+      }
+    }
+
+    // 监听事件
+    function addEvents(): void {
+      xhrequest.onreadystatechange = function handleLoad() {
+        if (xhrequest.readyState !== 4) {
+          return
+        }
+
+        // 网络出错或者超时
+        if (xhrequest.status === 0) {
+          return
+        }
+
+        const responseHeaders = parseHeaders(xhrequest.getAllResponseHeaders())
+        const responseData = responseType && responseType !== 'text' ? xhrequest.response : xhrequest.responseText
+        const response: TxiosResponse = {
+          data: responseData,
+          status: xhrequest.status,
+          statusText: xhrequest.statusText,
+          headers: responseHeaders,
+          config,
+          request: xhrequest
+        }
+
+        handleResponse(response)
+      }
+
+      // 网络错误处理
+      xhrequest.onerror = function() {
+        reject(createError(
+          'Network Error',
+          config,
+          null,
+          xhrequest
+        ))
+      }
+
+      xhrequest.ontimeout = function() {
+        reject(createError(
+          `Timeout of ${timeout} ms exceeded`,
+          config,
+          'ECONNABORTED',
+          xhrequest
+        ))
+      }
+
+      if (typeof onDownloadProgress === 'function') {
+        xhrequest.onprogress = onDownloadProgress
+      }
+
+      if (typeof onUploadProgress === 'function') {
+        xhrequest.upload.onprogress = onUploadProgress
+      }
+    }
+
+    // 请求头处理
+    function processHeaders(): void {
+      if (isFormData(data)) {
+        delete headers['Content-Type']
+      }
+
+      if ((withCredentials || isURLSameOrigin(url!)) && xsrfCookieName) {
+        const xsrfValue = cookie.read(xsrfCookieName)
+
+        if (xsrfValue) {
+          headers[xsrfHeaderName!] = xsrfValue
+        }
+      }
+
+      // 设置请求头
+      Object.keys(headers).forEach((key: string) => {
+        if (data === null && key.toLowerCase() === 'content-type') {
+          delete headers[key]
+        } else {
+          xhrequest.setRequestHeader(key, headers[key])
+        }
+      })
+    }
+
+    // 取消请求
+    function processCancel(): void {
+      if (cancelToken) {
+        // tslint:disable-next-line: no-floating-promises
+        cancelToken.promise.then(reason => {
+          xhrequest.abort()
+          reject(reason)
+        })
+      }
+    }
 
     // 处理响应状态
     function handleResponse(response: TxiosResponse) {
@@ -129,14 +158,6 @@ export default function xhr(config: TxiosRequestConfig): TxiosPromise {
           response
         ))
       }
-    }
-
-    if (cancelToken) {
-      // tslint:disable-next-line: no-floating-promises
-      cancelToken.promise.then(reason => {
-        xhrequest.abort()
-        reject(reason)
-      })
     }
   })
 }
